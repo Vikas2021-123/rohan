340c7bc6eb2235c725e541f7d30c44cb6ae7117e
vikas kumar gupta <vikaspe2021@gmail.com>
2021-09-15 15:19:21 +0530

    hi friends

---
+++ src/gitcode/github.java
@@ -0,0 +1,20 @@
+package gitcode;
+
+public class github {
+
+
+
+	
+
+		public static void main(String[] args) {
+		
+			System.out.println("This is my first git code");
+			
+			System.out.println("This is my second git code");
+
+			System.out.println("This is my fifth git code");
+
+		
+	}
+
+}
